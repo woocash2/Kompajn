69ba9571bcbab78eac07c7a03d5e55a2c17d69ae
marcinwasowicz <marcinkwasowicz@gmail.com>
2020-11-28 17:58:11 +0100

    Translation implemented.

---
+++ Kombajn/Assets/Scripts/CombainMover.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class CombainMover : MonoBehaviour {
+    public float translationSpeed;
+    public float angularSpeed;
+    public GameObject lookingPoint;
+
+    private static string vertical = "Vertical";
+    private static string horizontal = "Horizontal";
+
+    private Rigidbody rb;
+
+    private bool IsMoving(){
+        return Mathf.Approximately(0.0f, Input.GetAxis(vertical));
+    }
+
+    private bool IsRotating(){
+        return Mathf.Approximately(0.0f, Input.GetAxis(horizontal));
+    }
+
+    private void Translate(){
+        if(!IsMoving()){
+           rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+        else{
+            rb.velocity = (transform.position - lookingPoint.transform.position).normalized;
+        }
+    }
+
+    public void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Update(){
+        Translate();
+    }
+}
