7001710ca2f53842a72c3f184a66e393f2a4270c
Lukasz <lukasgniecki@gmail.com>
2020-11-28 21:43:51 +0100

    a little commit

---
+++ Kombajn/Assets/Scripts/CameraCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+
+    public float minDistance = 1f;
+    public float maxDistance = 8f;
+    public float smooth = 10f;
+
+    private Vector3 dollyDir;
+    private float distance;
+
+    private void Awake() {
+        dollyDir = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredCamPos = transform.parent.TransformPoint(dollyDir * maxDistance);
+
+        RaycastHit hit;
+        var layers = (~(1 << 9))&(~(1 << 10));
+        distance = maxDistance;
+        transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDir * distance, Time.deltaTime * smooth);
+    }
+}
---
+++ Kombajn/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [SerializeField] float camMoveSpeed = 120.0f;
+    public Transform follow;
+    [SerializeField] float hiClampAngle = 70.0f;
+    [SerializeField] float lowClampAngle = 50.0f;
+    [SerializeField] float sensitivity = 100f;
+
+    private float mouseX;
+    private float mouseY;
+
+    private float rotX = 0f;
+    private float rotY = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotX = rot.x;
+        rotY = rot.y;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * sensitivity * Time.deltaTime;
+        rotX += mouseY * sensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -hiClampAngle, lowClampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(-rotX, rotY, 0f);
+        transform.rotation = localRotation;
+    }
+
+    private void LateUpdate() {
+        CameraUpdater();
+    }
+
+    void CameraUpdater() {
+        float step = camMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, follow.position, step);
+    }
+
+    public void AddRecoil(float recoil) {
+        rotX += recoil;
+    }
+}
