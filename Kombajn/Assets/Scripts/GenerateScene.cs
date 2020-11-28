db936cacf0b06e89465caa45c39d94fd6f5eb4ad
marcinwasowicz <marcinkwasowicz@gmail.com>
2020-11-28 22:49:52 +0100

    Implemented

--- Kombajn/Assets/Scripts/GenerateScene.cs
+++ Kombajn/Assets/Scripts/GenerateScene.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class GenerateScene : MonoBehaviour
@@ -11,12 +12,26 @@ public class GenerateScene : MonoBehaviour
     public GameObject terrain;
 
     public int sizeX;
-    public int sizeY;
+    public int sizeZ;
+    public int grassCount;
+
+    private void fillSquare(int x, int z, int len, int count){
+        System.Random random = new System.Random();
+
+        for(int i = 0; i<count; i++){
+            int posX = random.Next(x, x + len);
+            int posZ = random.Next(z, z + len);
+
+            Instantiate(wheat, new Vector3(posX, 0.0f, posZ), Quaternion.identity);
+        }
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(terrain, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+        fillSquare(0, 0, sizeX, grassCount);
     }
 
     // Update is called once per frame
