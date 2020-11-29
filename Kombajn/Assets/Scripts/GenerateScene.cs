fcb29e703146479011d0b1ab9fdc6df6ab609958
Mozarek <karonkacper@gmail.com>
2020-11-29 01:14:18 +0100

    final

--- Kombajn/Assets/Scripts/GenerateScene.cs
+++ Kombajn/Assets/Scripts/GenerateScene.cs
@@ -12,8 +12,17 @@ public class GenerateScene : MonoBehaviour
     public GameObject steelFence;
     public GameObject player;
 
+    public int wheatCount;
+
     public int sizeX;
     public int sizeZ;
+
+    void generatePlayerFences(){
+        generateFencesAroundPlane(fence, new Vector3(sizeX/8f, 0f, sizeZ/8f), sizeX/4f, sizeZ/4f);
+        generateFencesAroundPlane(fence, new Vector3(sizeX/8f*5f, 0f, sizeZ/8f), sizeX/4f, sizeZ/4f);
+        generateFencesAroundPlane(fence, new Vector3(sizeX/8f, 0f, sizeZ/8f*5f), sizeX/4f, sizeZ/4f);
+        generateFencesAroundPlane(fence, new Vector3(sizeX/8f*5f, 0f, sizeZ/8f*5f), sizeX/4f, sizeZ/4f);
+    }
 
     void generateFencesAroundPlane(GameObject fencePrefab, Vector3 leftBackCorner, float lenX, float lenZ){
         float fenceLen = 4.4f; // how to read it from the object?
@@ -48,11 +57,11 @@ public class GenerateScene : MonoBehaviour
         terrain = Instantiate(terrain, new Vector3(sizeX/2.0f, 0, sizeZ/2.0f), Quaternion.identity);
         terrain.transform.localScale = new Vector3(sizeX*0.1f, 0.1f, sizeZ*0.1f); 
 
-        // generate steel fences around the plane
         generateFencesAroundPlane(steelFence, Vector3.zero, sizeX, sizeZ);
-        generateWheatInsidePlane(wheat, Vector3.zero, sizeX, sizeZ, 100);
+        generatePlayerFences();
+        generateWheatInsidePlane(wheat, Vector3.zero, sizeX, sizeZ, wheatCount);
 
-        player = Instantiate(player, new Vector3(3.0f, 0f, 3.0f), Quaternion.identity);
+        player = Instantiate(player, new Vector3(5.0f, 0f, 5.0f), Quaternion.Euler(0, 225, 0));
     }
 
     // Update is called once per frame
